5baf8f82df88583d75f0c2218c19858dd98f724d
Davide <davide02bragatto@gmail.com>
2025-03-16 16:07:24 +0100

    scoperta dei doxygen lol

--- include/driverCAN.h
+++ include/driverCAN.h
@@ -5,15 +5,42 @@
 
 #define CAN_QUEUE_SIZE 32
 
+/**
+ * @brief Struttura che rappresenta un messaggio CAN
+ */
 typedef struct {
-    uint32_t id;
-    uint8_t data[8];
-    uint8_t len;
+    uint32_t id;        /** identificatore del messaggio CAN */
+    uint8_t data[8];    /** Payload del messaggio CAN */
+    uint8_t len;        /** lunghezza in byte del payload */
 } CAN_Messaggio;
 
-void CAN_incoda(CAN_Messaggio msg);
-CAN_Messaggio CAN_toglicoda(void);
+/**
+ * @brief Inizializza la periferica FDCAN.
+ *
+ * Configura il bus CAN a 500 kbps, imposta i filtri, abilita gli interrupt,
+ * e configura i pin GPIO PA11 (RX) e PA12 (TX) per FDCAN1.
+ *
+ * Questa funzione deve essere chiamata prima di inviare o ricevere messaggi.
+ */
 void CAN_init(void);
+/**
+ * @brief Inserisce un messaggio nella coda circolare CAN.
+ *
+ * Questa funzione prende un messaggio di tipo `CAN_Messaggio` e lo inserisce
+ * nella coda circolare `canCoda`. Se la coda è piena, il messaggio più vecchio
+ * viene sovrascritto.
+ *
+ * @param msg Il messaggio CAN da inserire nella coda.
+ *
+ * @note La coda ha una dimensione massima definita da `CAN_QUEUE_SIZE`. Se la coda
+ *       è piena, il puntatore `tail` viene incrementato per eliminare il messaggio
+ *       più vecchio e fare spazio a quello nuovo.
+ */
+void CAN_Incoda(CAN_Messaggio msg);
+CAN_Messaggio CAN_toglicoda(void);
 void CAN_InviaMess();
 void CAN_RiceviMess();
-void CAN_
+void CAN_CheckFIfio();
+void ElaboraMsgRicevuto(uint32_t id, uint8_t *data, uint8_t length);
+
+#endif
--- src/driverCAN.c
+++ src/driverCAN.c
@@ -1,6 +1,7 @@
 #include "stm32g0xx_hal.h"
 #include "stm32g0xx_hal_fdcan.h"
 #include <string.h>
+#include "driverCAN.h"
 
 #define CAN_QUEUE_SIZE 32
 
@@ -11,5 +12,13 @@ typedef struct {
 } CAN_Messaggio;
 
 CAN_Messaggio canCoda[CAN_QUEUE_SIZE];
-uint8_t head = 0, tail = 0;
+uint8_t head = 0;
+uint8_t tail = 0;
 
+void CAN_incoda(CAN_Messaggio msg) {//prende un msg di tipo CAn_Messaggio e lo inserisce in una coda circolare (canCoda)
+    canCoda[head]=msg;              //copia il msg nella posiione attuale, quindi in head
+    head=(head+1)%CAN_QUEUE_SIZE;   //incrementa head, e nel caso raggiunga il limite della coda, lo riporta a 0 (%=divisione con resto 0)
+    if(head==tail){                 //controllo overflow (per preparare la sovrascrittura)
+        tail(tail+1)%CAN_QUEUE_SIZE;//incrementa tail, eliminando il messaggio più vecchio (e fa spazio a quello nuovo)
+    }
+}
